a325a2060839a36a47ea85671bd3af40ff57497a
Elisheba Anderson <elishebaanderson@Elishebas-MacBook-Pro.local>
2019-05-02 22:55:30 -0400

    Adding class, service and resource for courses

---
+++ src/test/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/SpringBootFullstackCrudFullStackWithMavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minutes.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class SpringBootFullstackCrudFullStackWithMavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
